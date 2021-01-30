47c32ec30dfb7338287efb32bc7e271fb5c7a35b
Siddharth-Gandhi <ssgandhi602@gmail.com>
2021-01-30 19:26:16 +0530

    Second commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const birthYear = 2001;
+const currentYear = new Date().getFullYear();
+const age =
+	new Date() > new Date(`11/24/${currentYear}`)
+		? currentYear - birthYear
+		: currentYear - birthYear - 1;
+document.getElementById("age").innerText = `${age}`;
